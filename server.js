6bdf1d207cb5f5a32f93e853aa8ac2b0d2a8a096
Laurie Corrin <laurie.corrin@multiverse.io>
2025-05-20 16:30:46 +0000

    removed static server and added CRUD with Users in sqlite

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./src/app')
+const { db } = require('./db/connection')
 const port = 3000
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await db.sync();
     console.log(`server is listening at ${port}`)
 })
--- src/app.js
+++ src/app.js
@@ -1,13 +1,93 @@
-const express = require("express")
+const express = require("express");
+const User = require("./models/User");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 const app = express();
 
 // add all my callbacks
 
-app.use("/static", express.static("public"))
+app.use(express.json())
 
-app.use("/", (req, res) => {
-    const msg = `this was a ${req.method} request`
-    res.send(msg)
+// CRUD
+// Create = POST
+app.post("/users", async(req, res, next) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch(error) {
+        next(error);
+    }
 })
+
+
+// Read = GET
+// GET mysite.com/users
+app.get("/users", async (req, res, next) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (error) {
+        next(error);
+    }
+})
+
+app.get("/users/:username", async (req, res, next) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                username: req.params.username
+            }
+        });
+        res.json(user);
+    } catch (error) {
+        next(error);
+    }
+})
+
+
+// Update = PUT (PATCH)
+// UPDATE mysite.com/users/username (provide body)
+app.put("/users/:username", async(req, res, next) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                username: req.params.username
+            }
+        });
+        if (user) {
+            user = await user.update(req.body);
+            res.json(user);
+        } else {
+            res.status(404).send("user not found")
+        }
+    } catch (error) {
+        next(error);
+    }
+})
+
+
+
+// Delete = DELETE
+// DELETE mysite.com/users/badactor
+app.delete("/users/:username", async (req, res, next) => {
+    // alternative to finding user and user.destroy is
+    // User.destroy(where)
+    try {
+        const user = await User.findOne({
+            where: {
+                username: req.params.username
+            }
+        });
+        if (user) {
+            const userDeleted = await user.destroy();
+            res.json(userDeleted);
+        } else {
+            res.status(404).send("user not found")
+        }
+    } catch(error) {
+        next(error);
+    }
+})
+
 
 module.exports = app;
